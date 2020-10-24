cdd7aed8c46f814f437e2d0c5965bb2ba411e886
sehee0531 <hello.git.GitHub@gmail.com>
2020-10-24 16:42:01 +0900

    FlowLayout

---
+++ FlowLayoutEX/src/FlowLayoutEX.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class FlowLayoutEX extends JFrame {
+	public FlowLayoutEX() {
+		setTitle("FlowLayout Sample");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		
+		c.setLayout(new FlowLayout(FlowLayout.LEFT,30,40));
+		c.add(new JButton("add"));
+		c.add(new JButton("sub"));
+		c.add(new JButton("mul"));
+		c.add(new JButton("div"));
+		c.add(new JButton("Calculate"));
+		
+		setSize(300,200);
+		setVisible(true);
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new FlowLayoutEX();
+
+	}
+
+}
