3952b5edf9f45a70d5a5bd12fb09a8b5177de620
Jung Se Hee <61817204+sehee0531@users.noreply.github.com>
2020-10-24 16:57:01 +0900

    FlowLayout 연습

--- FlowLayoutEX/src/FlowLayoutEX.java
+++ FlowLayoutEX/src/FlowLayoutEX.java
@@ -7,12 +7,12 @@ public class FlowLayoutEX extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container c = getContentPane();
 		
-		c.setLayout(new FlowLayout(FlowLayout.LEFT,30,40));
+		c.setLayout(new FlowLayout(FlowLayout.LEFT,30,20));
 		c.add(new JButton("add"));
-		c.add(new JButton("sub"));
-		c.add(new JButton("mul"));
 		c.add(new JButton("div"));
 		c.add(new JButton("Calculate"));
+		c.add(new JButton("mul"));
+		c.add(new JButton("sub"));
 		
 		setSize(300,200);
 		setVisible(true);
